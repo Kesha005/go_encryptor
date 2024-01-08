a57fa16f0439712f2f3c4c349c5df12113b8f053
Kerimberdi Saparow <keshasaparow@mail.ru>
2024-01-08 19:06:01 +0500

    autogenerate_plan

--- token/token.go
+++ token/token.go
@@ -1,6 +1,5 @@
 package token
 
-
 import (
 	"fmt"
 	"time"
@@ -8,5 +7,18 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
+var secret = []byte("121212121")
 
+func GenerateToken(id ) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"data": data,
+			"exp":  time.Now().Add(time.Hour * 24).Unix(),
+		})
+	tokenString ,err:= token.SignedString(secret)
+	if err !=nil{
+		return "There are some errors", err
+	}
+	return tokenString, nil
 
+}
