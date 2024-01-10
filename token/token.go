242abf93d6bc9c4832471ec00d46c6dc06254950
Kerimberdi Saparow <keshasaparow@mail.ru>
2024-01-10 13:42:35 +0500

    token_get_claims

--- token/token.go
+++ token/token.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"errors"
 	"os"
 	"time"
 
@@ -8,38 +9,65 @@ import (
 )
 
 
-func GenerateToken(id int, username string ) (string, error) {
-	var secret = []byte(os.Getenv("SECRET_KEY"))
+type User struct {
+	id int
+	username string
+}
+
+func ReturnSecret() []byte {
+	token := []byte(os.Getenv("SECRET_KEY"))
+	return token
+}
+
+func GenerateToken(id int, username string) (string, error) {
+	var secret = ReturnSecret()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
 		jwt.MapClaims{
-			"id":id ,
-			"username":username,
-			"exp":  time.Now().Add(time.Hour * 24).Unix(),
+			"id":       id,
+			"username": username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
 		})
-	tokenString ,err:= token.SignedString(secret)
-	if err !=nil{
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
 		return "There are some errors", err
 	}
 	return tokenString, nil
 
 }
 
-func ControlToken(input_token string)(string,error){
-	var secret = []byte(os.Getenv("SECRET_KEY"))
-	token, err := jwt.Parse(input_token, func(token *jwt.Token) (interface{},error){
-		return secret,nil
+func ControlToken(input_token string) (string, error) {
+	var secret =  ReturnSecret()
+	token, err := jwt.Parse(input_token, func(token *jwt.Token) (interface{}, error) {
+		return secret, nil
 	})
 
-	if err!= nil{
-		return "There are error in token",err
+	if err != nil {
+		return "There are error in token", err
 	}
-	if !token.Valid{
-		return "Invalid token",err
+	if !token.Valid {
+		return "Invalid token", err
 	}
 
 	return "It is ok ", nil
 }
 
-func GetTokenData(tokenString string) {
-	
+func GetTokenData(tokenString string)(User,error){
+	var secret =  ReturnSecret()
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secret, nil
+	})
+	if err != nil {
+		return User{}, err
+	}
+	if !token.Valid {
+		return User{}, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		username := claims["username"].(string)
+		id := claims["id"].(int)
+		return User{id: id, username: username}, nil
+	}
+	return User{},errors.New("It is invalid token")
 }
+
