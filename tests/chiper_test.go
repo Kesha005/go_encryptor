c576ca35d3d04bdf41f4738eca37049cfc3e1d0d
Kerimberdi Saparow <keshasaparow@mail.ru>
2024-01-10 15:46:12 +0500

    chiper_test_passed_fixed_bug

---
+++ tests/chiper_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Kesha005/go_encryptor"
+	"github.com/joho/godotenv"
+)
+
+
+func TestChiper(t *testing.T){
+	godotenv.Load("../.env")
+	
+	var word string = "Hello world"
+	chiped,err := go_encryptor.Encrypt(word)
+	if err!=nil{
+		fmt.Println(err)
+	}
+	dechiped,deeer := go_encryptor.Decrypt(chiped)
+	if deeer !=nil{
+		fmt.Println(deeer.Error())
+	}
+	if dechiped != word {
+		t.Error("It is not word which we chiped")
+	}
+
+}
