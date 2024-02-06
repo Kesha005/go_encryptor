10886a88f0acf308662d75b33e256d3516b9807e
Kerimberdi Saparow <keshasaparow@mail.ru>
2024-02-06 13:12:39 +0500

    changed Chiper alg

--- token/token.go
+++ token/token.go
@@ -27,7 +27,7 @@ func ReturnSecret() []byte {
 
 func (user UserToken)GenerateToken() (string, error) {
 	var secret = ReturnSecret()
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+	token := jwt.NewWithClaims(&jwt.SigningMethodHMAC{},
 		jwt.MapClaims{
 			"id":       user.Id,
 			"username": user.Username,
