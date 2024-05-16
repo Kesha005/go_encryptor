aa14078af94c6b70df239fcfa33c62a27da07584
Kesha005 <backendtm005@gmail.com>
2024-05-16 14:51:54 +0500

    fixed composition bug

--- go_encryptor.go
+++ go_encryptor.go
@@ -7,12 +7,11 @@ import (
 	"os"
 )
 
-
-func Encode(b []byte) string {
+func encode(b []byte) string {
 	return base64.StdEncoding.EncodeToString(b)
-	
+
 }
-func Decode(s string) []byte {
+func decode(s string) []byte {
 	data, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
 		panic(err)
@@ -22,35 +21,39 @@ func Decode(s string) []byte {
 
 // Encrypt method is to encrypt or hide any classified text
 func Encrypt(text string) (string, error) {
-	
-var bytes = []byte(os.Getenv("IV_16_KEY"))
 
-// This should be in an env file in production
-var MySecret string = os.Getenv("SECRET_KEY")
+	var bytes = []byte(os.Getenv("IV_16_KEY"))
+
+	// This should be in an env file in production
+	var MySecret string = os.Getenv("SECRET_KEY")
 
 	block, err := aes.NewCipher([]byte(MySecret))
 	if err != nil {
 		return "", err
 	}
+
 	plainText := []byte(text)
 	cfb := cipher.NewCFBEncrypter(block, bytes)
+
 	cipherText := make([]byte, len(plainText))
+
 	cfb.XORKeyStream(cipherText, plainText)
-	return Encode(cipherText), nil
+
+	return encode(cipherText), nil
 }
 
 // Decrypt method is to extract back the encrypted text
 func Decrypt(text string) (string, error) {
-	
-var bytes = []byte(os.Getenv("IV_16_KEY"))
 
-// This should be in an env file in production
-var MySecret string = os.Getenv("SECRET_KEY")
+	var bytes = []byte(os.Getenv("IV_16_KEY"))
+
+	// This should be in an env file in production
+	var MySecret string = os.Getenv("SECRET_KEY")
 	block, err := aes.NewCipher([]byte(MySecret))
 	if err != nil {
 		return "", err
 	}
-	cipherText := Decode(text)
+	cipherText := decode(text)
 	cfb := cipher.NewCFBDecrypter(block, bytes)
 	plainText := make([]byte, len(cipherText))
 	cfb.XORKeyStream(plainText, cipherText)
